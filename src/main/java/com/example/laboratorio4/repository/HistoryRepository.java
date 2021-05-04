922b3fcb4fb52b074fb6fa0e205a90a995c0b266
Ali Lopez <a20167442@pucp.edu.pe>
2021-05-03 21:27:00 -0500

    search

--- src/main/java/com/example/laboratorio4/repository/HistoryRepository.java
+++ src/main/java/com/example/laboratorio4/repository/HistoryRepository.java
@@ -2,6 +2,7 @@ package com.example.laboratorio4.repository;
 
 import com.example.laboratorio4.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.List;
 @Repository
 public interface HistoryRepository extends JpaRepository<History,Integer> {
 
-    //COMPLETAR
+    @Query(value = "SELECT * FROM job_history\n" +
+            "WHERE employee_id NOT IN (SELECT max(salary) FROM employees);",
+            nativeQuery = true)
+    List<History> listaEmpleadosMayorSalario();
 
 }
