ddde789726fe42aae3671b112c9d110e18937919
Katherine Obregon <a20181823@pucp.edu.pe>
2021-05-03 23:50:08 -0500

    validaciones

--- src/main/java/com/example/laboratorio4/controller/HistoryController.java
+++ src/main/java/com/example/laboratorio4/controller/HistoryController.java
@@ -1,6 +1,9 @@
 package com.example.laboratorio4.controller;
 
+import com.example.laboratorio4.repository.HistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/history")
 public class HistoryController {
 
+    @Autowired HistoryRepository historyRepository;
+
     @GetMapping(value = {"","/"})
-    public String historialEmpleado(){
-        return "nada";
+    public String historialEmpleado(Model model){
+        model.addAttribute("lista", historyRepository.listaEmpleadosMayorSalario());
+        return "history/lista";
+
     }
 
 
